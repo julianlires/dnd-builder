bc63222ca8f211a80b7f90c249108065a5d3eeba
Julián Lires <julianlires@gmail.com>
2025-02-20 16:35:20 -0300

    feat: :sparkles: redirect index to character-sheet page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/character-sheet',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
